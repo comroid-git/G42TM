dd72f7c5e800a19334f8f339f1b056a143e25092
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2022-10-19 23:30:56 +0200

    basic impl

---
+++ G42TM/GrammarListener.cs
@@ -0,0 +1,14 @@
+﻿using G42TM.Antlr;
+using TextMateSharp.Grammars;
+
+namespace G42TM;
+
+public class GrammarListener : ANTLRv4ParserBaseListener
+{
+    public GrammarDefinition Definition { get; }
+
+    public GrammarListener(GrammarDefinition definition)
+    {
+        Definition = definition;
+    }
+}
